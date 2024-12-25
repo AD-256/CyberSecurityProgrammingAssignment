d5d1d5b3fd3dc2a6b24c09b503756bd24d9322c0
AD-256 <AlfieBusiness06@gmail.com>
2024-12-25 04:51:39 +0000

    Tweaks

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
-import re
-import sqlite3
-import functions
+import re, sqlite3, magics, bcrypt
 
-userDataDB = sqlite3.connect("userData.db")
+userDataDB = sqlite3.connect("userData.txt")
+regexStrong = re.compile(r"(?=.{13,})(?=(.*[a-z]){2,})(?=(.*[A-Z]){2,})(?=(.*\d){2,})(?=(.*[^\w\d\s@#]){2,}).*$", re.MULTILINE | re.UNICODE)
+regexWeak = re.compile(r"(?=.{8,})(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*\d){1,})(?=(.*[^\w\d\s@#]){1,}).*$", re.MULTILINE | re.UNICODE)
 
-print ("Would you like to ")
-functions.ynInput()
+print ("Are you logging in?")
+loginCheck = magics.ynInput()
 
-print("Input your new password")
-passwordAttempt = input()
+if loginCheck == True:
+    pass
+elif loginCheck == False:
+    print("Input your new username")
+    userNameInput = str(input())
+    print("Input your new password")
+    passwordAttempt = input()
 
-if re.search(".{13,}",passwordAttempt):#Strong
-    print("Strong")
-elif re.search("",passwordAttempt):#Weak
-    print("weak")
-else :#Invalid
-    print("inv")
+    if regexStrong.match(passwordAttempt) != None:#Strong
+        print("Strong")
+    elif regexWeak.match(passwordAttempt) != None:#Weak
+        print("weak")
+    else:#Invalid
+        print("invalid")
+    #userDataDB.s
+else:
+    raise ValueError("WTF?!?!?!?")
